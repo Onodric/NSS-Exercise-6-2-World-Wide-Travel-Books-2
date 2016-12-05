243c08b1aa6b17a428cb21b91b4182641faa4430
Ben Marks <onodric@yahoo.com>
2016-12-05 13:52:24 -0600

    finished factory implementation
    also cleaned up con logs

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -1,44 +1,19 @@
 "use strict";
 
-app.controller('bookCtrl', ($scope) => {
+app.controller('bookCtrl', ($scope, BookStorage) => {
   $scope.title = "Angular Book List";
 
   $scope.books = [];
-// console.log("books init: ");
 
-  let setBooks = (bookArr) => {
-// console.log("setBooks called with: ", bookArr);
-    $scope.books = bookArr.guides;
-// console.log("$scope.books: ", $scope.books);
-  };
-// console.log("setBooks defined: ");
-
-  $scope.loadBooks = () => {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: '../data/guides.json',
-      })
-      .done(function(results) {
-        resolve(results);
-// console.log("success", results);
-        // setBooks(results.guides);
-        // return results.guides;
-      })
-      .fail(function() {
-        console.log("error");
-      })
-      .always(function() {
-        console.log("complete");
-      });
-    });
-  };
-  
-  $scope.loadBooks()
-  .then((data) => {
+  BookStorage.loadBooks()
+  .then((data)=>{
     setBooks(data);
-console.log("books: ", $scope.books);
+    // $scope.books = data;
     $scope.$apply();
   });
 
+  let setBooks = (bookArr) => {
+    $scope.books = bookArr.guides;
+  };
 
 });
